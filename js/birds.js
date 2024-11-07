d70c4bb647ea7a6420f014cc4a79322f43faf87c
carotproject <carotproject@gmail.com>
2024-11-06 22:30:39 -0500

    make changes to talks

--- js/birds.js
+++ js/birds.js
@@ -32,11 +32,16 @@ function squares(p) {
     const displayDelay = 500;
     let isDrawing = false;
 
-    const folders = ['church/1/', 'church/2/', 'church/3/','church/4/','church/5/',
-        'church/6/','church/7/','church/8/','church/9/','church/10/','church/11/',
-        'church/12/','church/13/','church/14/','church/15/','church/16/','church/17/',
-        'church/18/','church/19/','church/20/','church/21/','church/22/',
-      'church/24/','church/26/'];
+    const folders = [
+        'church/1/', 'church/2/', 'church/3/', 'church/4/', 'church/5/',
+        'church/6/', 'church/7/', 'church/8/', 'church/9/', 'church/10/',
+        'church/11/', 'church/12/', 'church/13/', 'church/14/', 'church/15/',
+        'church/16/', 'church/17/', 'church/18/', 'church/19/', 'church/20/',
+        'church/21/', 'church/22/', 'church/24/', 'church/26/',
+        // Add 'church/30/' multiple times to increase its likelihood
+        'church/30/', 'church/30/', 'church/30/','church/30/', 'church/31/', 'church/31/','church/31/', 'church/31/'
+    ];
+
     const imageName = 'church-doors.png';
 
     setup();
